85d38e974586638aca725621f3ce4fcff193efd4
helloprof <suluxan.seneca@gmail.com>
2023-11-20 15:34:08 -0500

    adding sequelize to data module and some refactoring of functions

--- showService.js
+++ showService.js
@@ -1,16 +1,62 @@
 const videos = require("./data/videos.json")
 const channels = require("./data/channels.json")
+const env = require("dotenv")
+env.config()
+
+const Sequelize = require('sequelize');
+let sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+    host: process.env.PGHOST,
+    dialect: 'postgres',
+    port: 5432,
+    dialectOptions: {
+        ssl: { rejectUnauthorized: false },
+    },
+});
+
+
+const Channel = sequelize.define('Channel', {
+    channelID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true, // use "project_id" as a primary key
+        autoIncrement: true, // automatically increment the value
+    },
+    name: Sequelize.STRING,
+    image: Sequelize.STRING
+})
+
+const Video = sequelize.define('Video', {
+    videoID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true, // use "project_id" as a primary key
+        autoIncrement: true, // automatically increment the value
+    },
+    title: Sequelize.STRING,
+    videoURL: Sequelize.STRING,
+    date: Sequelize.DATE,
+    thumbnail: Sequelize.STRING
+})
+
+Video.belongsTo(Channel, {foreignKey: "channelID"})
 
 function initialize() {
     return new Promise((resolve, reject) => {
-        console.log("combine your json files together into a new array")
-        resolve()
+        sequelize.sync().then(() => {
+            console.log("connected to DB")
+            resolve()
+
+        }).catch((err) => {
+            reject(err)
+        })
+
     })
     // combine themes json and legosets json into a new js array
     // filteredArray
-    
+
     // TODO
 }
+
+
+
 
 function getAllChannels() {
     return new Promise((resolve, reject) => {
@@ -41,7 +87,17 @@ function getVideosByChannel(channelID) {
             resolve(result)
         } else {
             reject("video not found!")
-        }    
+        }
+    })
+}
+
+function addChannel(formData) {
+    return new Promise((resolve, reject) => {
+        Channel.create(formData).then(() => {
+            resolve()
+        }).catch((err) => {
+            reject(err)
+        })
     })
 }
 
@@ -50,5 +106,6 @@ module.exports = {
     initialize,
     getAllChannels,
     getVideoByID,
-    getVideosByChannel
+    getVideosByChannel,
+    addChannel
 }
